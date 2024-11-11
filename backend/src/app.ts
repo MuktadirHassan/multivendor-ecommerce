4427a77d963b79cb563c66476b75869404c2e4cf
MuktadirHassan <tamimmuktadir@gmail.com>
2024-11-11 16:32:39 +0600

    feat: add gemini based product search

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,13 @@ import { handleGlobalError } from "./utils/Error";
 
 const app = express();
 
+import productRoutes from "./routes/product.routes";
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.status(200);
 });
+
+app.use("/api/products", productRoutes);
 
 app.use(handleGlobalError);
 
