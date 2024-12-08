2c4eb13f71e13f6fa2d78a7c133431e1a65af2cf
MuktadirHassan <tamimmuktadir@gmail.com>
2024-12-09 00:43:11 +0600

    feat: implement review management API with CRUD operations and update shop management API

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,27 +1,30 @@
 import express from "express";
 import morgan from "morgan";
 
-import { handleGlobalError } from "./utils/Error";
-import { PrismaClient } from "@prisma/client";
-import { AuthRepository } from "./repositories/auth.repository";
-import { UserRepository } from "./repositories/user.repository";
-import { AuthService } from "./services/auth.service";
-import { AuthController } from "./controllers/auth.controller";
-import { AuthMiddleware } from "./middleware/auth.middleware";
-import { createAuthRouter } from "./routes/auth.routes";
-import { createProductRouter } from "./routes/product.routes";
-import { ProductController } from "./controllers/product.controller";
-import { ProductRepository } from "./repositories/product.repository";
-import { ProductService } from "./services/product.service";
 import { db } from "./config/database";
-import { ShopRepository } from "./repositories/shop.repository";
-import { ShopService } from "./services/shop.service";
-import { ShopController } from "./controllers/shop.controller";
-import { createShopRouter } from "./routes/shop.routes";
-import { CategoryRepository } from "./repositories/category.repository";
-import { CategoryService } from "./services/category.service";
+import { AuthController } from "./controllers/auth.controller";
 import { CategoryController } from "./controllers/category.controller";
+import { ProductController } from "./controllers/product.controller";
+import { ReviewController } from "./controllers/review.controller";
+import { ShopController } from "./controllers/shop.controller";
+import { AuthMiddleware } from "./middleware/auth.middleware";
+import { AuthRepository } from "./repositories/auth.repository";
+import { CategoryRepository } from "./repositories/category.repository";
+import { ProductRepository } from "./repositories/product.repository";
+import { ReviewRepository } from "./repositories/review.repository";
+import { ShopRepository } from "./repositories/shop.repository";
+import { UserRepository } from "./repositories/user.repository";
+import { createAuthRouter } from "./routes/auth.routes";
 import { createCategoryRouter } from "./routes/category.routes";
+import { createProductRouter } from "./routes/product.routes";
+import { createReviewRouter } from "./routes/review.routes";
+import { createShopRouter } from "./routes/shop.routes";
+import { AuthService } from "./services/auth.service";
+import { CategoryService } from "./services/category.service";
+import { ProductService } from "./services/product.service";
+import { ReviewService } from "./services/review.service";
+import { ShopService } from "./services/shop.service";
+import { handleGlobalError } from "./utils/Error";
 
 const userRepository = new UserRepository(db);
 const authRepository = new AuthRepository(db, userRepository);
@@ -41,6 +44,14 @@ const categoryRepository = new CategoryRepository(db);
 const categoryService = new CategoryService(categoryRepository);
 const categoryController = new CategoryController(categoryService);
 
+const reviewRepository = new ReviewRepository(db);
+const reviewService = new ReviewService(
+  reviewRepository,
+  productRepository,
+  shopRepository
+);
+const reviewController = new ReviewController(reviewService);
+
 const app = express();
 
 app.use(express.json());
@@ -54,6 +65,7 @@ app.use(
   "/categories",
   createCategoryRouter(categoryController, authMiddleware)
 );
+app.use("/reviews", createReviewRouter(reviewController, authMiddleware));
 
 app.use(handleGlobalError);
 
